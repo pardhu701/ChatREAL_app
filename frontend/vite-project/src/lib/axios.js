0f28c0f10ba8aa604a0a1bc3aac6d3559cd51608
pardhu701 <104940825+pardhu701@users.noreply.github.com>
2026-01-23 15:22:26 +0530

    Fix baseURL in axios instance configuration

--- frontend/vite-project/src/lib/axios.js
+++ frontend/vite-project/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
    
-    baseURL: 'http://localhost:5001/apihttps://us-east-1.console.aws.amazon.com/elasticbeanstalk/home?region=us-east-1#/environment/dashboard?environmentId=e-mkn8bfn2eq/api',
+    baseURL: 'https://us-east-1.console.aws.amazon.com/elasticbeanstalk/home?region=us-east-1#/environment/dashboard?environmentId=e-mkn8bfn2eq/api',
     withCredentials:true
    
    
