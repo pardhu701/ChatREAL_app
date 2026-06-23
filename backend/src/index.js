53b82f1e9a6b22c4673a43f1b16c1941f2a90d23
pardhu701 <104940825+pardhu701@users.noreply.github.com>
2026-06-24 04:04:00 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: 'https://34.228.80.120:3001',  // replace with your frontend URL
+    origin: 'http://34.228.80.120:3001',  // replace with your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // <-- allow cookies/auth headers
@@ -31,7 +31,7 @@ app.use(cors({
 
 // Handle preflight requests
 app.options('*', cors({
-    origin: 'https://34.228.80.120:3001',
+    origin: 'http://34.228.80.120:3001',
     credentials: true
 }));
 
