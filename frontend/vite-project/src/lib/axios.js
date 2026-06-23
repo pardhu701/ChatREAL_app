b75c067d14ba1bc8cba0afc9e15965b225873584
pardhu701 <104940825+pardhu701@users.noreply.github.com>
2026-06-24 04:43:20 +0530

    Change axios baseURL from HTTPS to HTTP

--- frontend/vite-project/src/lib/axios.js
+++ frontend/vite-project/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
    
-    baseURL: 'https://34.228.80.120:5001/api',
+    baseURL: 'http://34.228.80.120:5001/api',
     withCredentials:true
    
    
